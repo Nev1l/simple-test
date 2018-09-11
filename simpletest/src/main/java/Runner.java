f8c5323c767a39a45e31ae4b10d016082d7ff769
Nev1l <vik-kopachev@yandex.ru>
2018-09-11 20:33:39 +0300

    Master init

---
+++ simpletest/src/main/java/Runner.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Администратор on 11.09.2018.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ simpletest/src/main/java/com/component/bean/Field.java
@@ -0,0 +1,18 @@
+package com.component.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Администратор on 11.09.2018.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Field {
+    private String name;
+    private String type;
+}
